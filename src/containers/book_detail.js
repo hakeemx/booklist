fdbc3f2d816a21aa818d25665877709bdc6e4819
hxk33m <hxk33m@gmail.com>
2017-07-14 16:25:14 -0400

    Push finished app

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,8 +1,25 @@
 import React, {Component} from 'react';
-export default class BookDetail extends Component{
+import {connect} from 'react-redux';
+
+class BookDetail extends Component{
 	render(){
+		if(!this.props.book){
+			return <div>Select a book to get started.</div>;
+		}
 		return (
-			<div>Book Detail!</div>
+			<div>
+				<h3>Details for:</h3>
+				<div>Title: {this.props.book.title}</div>
+				<div>Pages: {this.props.book.pages}</div>
+			</div>
 		);
 	}
 }
+
+function mapStateToProps(mapStateToProps) {
+	return{
+		book:state.activeBook
+	}
+}
+
+export default connect(mapStateToProps)(BookDetail);
